f6d2f65e27f55a0fc183684e82823bea6265a48c
Yaki55 <mely020202@gs.utm.mx>
2024-06-09 14:58:32 -0600

    enviroment

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import userRoutes from './routes/userRoutes.js'; // Asegúrate de que estas rutas existan
-import contactRoutes from './routes/contactRoutes.js'; // Asegúrate de que estas rutas existan
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
+
+// Configurar dotenv
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB Atlas
+// Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Could not connect to MongoDB Atlas', err));
 
-// Routes
+// Rutas
 app.use('/api/users', userRoutes);
 app.use('/api/contacts', contactRoutes);
 
